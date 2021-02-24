459b2e4926b49f24537b51030cadf2ed10a12ad7
VirtualThug9 <74547812+VirtualThug9@users.noreply.github.com>
2021-02-24 14:12:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import pyautogui as pag
-import time
+import pyautogui as pag # importing pyautogui with the keyword "pag" so that we wouldn't need to enter such a lengthy name again and again
+import time # importing the package called time
 
-msg = "Subscribe to VirtualThug9!"
+msg = "Subscribe to VirtualThug9!" # creating a variable to spam
 
-time.sleep(5)
+time.sleep(5) # making the code pause for a few secs (5 secs in my case)
 
-while True:
-	pag.typewrite(msg)
-	pag.press("enter")
+while True: # creating an infinte loop; this is infinite because True is always == True. So, there is no condition to set True == False
+	pag.typewrite(msg) # typing where our cursor is using pyautogui
+	pag.press("enter") # pressing the key "enter" using pyautogui
